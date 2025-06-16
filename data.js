a33ae51af164a1e74666024f03b85dd45341bd20
linuxtat <138621875+linuxtat@users.noreply.github.com>
2025-06-16 16:48:29 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const users = {
+  "user1": {
+    password: "pass1",
+    name: "রহিম",
+    investment: 10000,
+    profitRate: 2, // percent per day
+    createdAt: "2024-06-01"
+  },
+  "user2": {
+    password: "pass2",
+    name: "করিম",
+    investment: 5000,
+    profitRate: 1.5,
+    createdAt: "2024-06-10"
+  }
+};
